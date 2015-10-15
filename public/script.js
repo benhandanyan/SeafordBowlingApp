40358661259399ff46593b61cea7bcd66602f333
Benjamin Handanyan <benhandanyan@gmail.com>
2015-10-15 15:09:18 +0000

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,363 @@
+// script.js
+    // create the module and name it scotchApp
+    var bapp = angular.module('bowling', ['ui.router']);
+
+  // configure our routes
+	bapp.config([
+		'$stateProvider',
+		'$urlRouterProvider',
+		function($stateProvider, $urlRouterProvider) {
+			$stateProvider
+				.state('form', {
+					url: '/ballform',
+					templateUrl: "/ballform.html",
+					controller: "inputController",
+					onEnter: ['$state', 'auth', function($state, auth){
+					    if(!auth.isLoggedIn()){
+					      $state.go('login');
+					    }
+  					}]
+				})
+				.state('login', {
+					url: '/login',
+					templateUrl: "/login.html",
+					controller: "AuthCtrl",
+					onEnter: ['$state', 'auth', function($state, auth){
+					    if(auth.isLoggedIn()){
+					      $state.go('form');
+					    }
+  					}]
+				})
+				.state('search', {
+					url: '/search',
+					templateUrl: "/searchresults.html",
+					controller: "searchCtrl",
+					onEnter: ['$state', 'auth', function($state, auth){
+					    if(!auth.isLoggedIn()){
+					      $state.go('login');
+					    }
+  					}],
+					resolve : {
+						postPromise: ["factory", function(factory){
+							return factory.fetchAll();
+						}]
+					}
+				})
+				.state('ball', {
+					url: '/info/{id}',
+					templateUrl: '/ball.html',
+					controller: "queryController",
+					resolve: {
+						post: ['$stateParams', 'factory', function($stateParams, factory) {
+							return factory.fetchOne($stateParams.id);
+						}]
+					},
+					onEnter: ['$state', 'auth', function($state, auth){
+					    if(!auth.isLoggedIn()){
+					      $state.go('login');
+					    }
+  					}]
+				})
+				$urlRouterProvider.otherwise('login');
+		}
+		
+	]);
+	
+bapp.service('searchService', function(){
+	var name = '';
+	return {
+		getText: function() {
+			return name;
+		},
+		setText: function(value) {
+			name = value;
+		}
+	}
+});
+
+bapp.factory('factory', ["$http", function($http){
+
+  var object = {
+    factory : [],
+    fetchHolder: [],
+    //Now we call the API as it was laid out in the server file.
+
+    createInfo : function(newInfo) {
+        return $http.post('/info', newInfo).success(function(data){
+            object.factory.push(data);
+        });
+    },
+
+    fetchOne : function(id) {
+        return $http.get("/info/"+id).success(function(data){  angular.copy(data, object.fetchHolder); });
+    },
+
+    fetchAll : function() {
+    	return $http.get("/info").success(function(data){
+    		angular.copy(data, object.factory);
+    	});
+    } 
+    
+    
+  };
+
+  return object;
+
+}]);
+
+bapp.factory('auth', ['$http', '$window', '$state', function($http, $window, $state){
+   var auth = {
+  saveToken : function (token){
+  $window.localStorage['bowling-token'] = token;
+},
+
+getToken : function (){
+  return $window.localStorage['bowling-token'];
+},
+isLoggedIn : function(){
+  var token = auth.getToken();
+  if(token){
+    var payload = JSON.parse($window.atob(token.split('.')[1]));
+    return payload.exp > Date.now() / 1000;
+  } else {
+    return false;
+  }
+},
+currentUser : function(){
+  if(auth.isLoggedIn()){
+    var token = auth.getToken();
+    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+    return payload.username;
+  }
+},
+logIn : function(user){
+  return $http.post('/login', user).success(function(data){
+    auth.saveToken(data.token);
+  });
+},
+logOut : function(){
+  $window.localStorage.removeItem('bowling-token');
+  $state.go("login");
+}
+};
+  return auth;
+}])
+
+bapp.controller(
+	"inputController",
+	["$scope", '$state', "factory", "searchService", "auth", function($scope, $state, factory, searchService, auth){
+		$scope.logOut = auth.logOut;
+		$scope.isLoggedIn = auth.isLoggedIn;
+  		$scope.currentUser = auth.currentUser;
+		$scope.search = function() {
+			searchService.setText($scope.searchtext);
+			$state.go('search');
+		}
+		$scope.submit = function(){
+			if($scope.name && $scope.name != "" && $scope.date && $scope.date != "" && $scope.address && $scope.address != "" && $scope.city && $scope.city != "" 
+			&& $scope.state && $scope.state != "" && $scope.zip && $scope.zip != "" && $scope.phone && $scope.phone != "" && $scope.cell && $scope.cell != "" 
+			&& $scope.email && $scope.email != "" && $scope.hand && $scope.hand != "" && $scope.conv && $scope.conv != "" && $scope.ft && $scope.ft != "" && $scope.ball && $scope.ball != ""
+			&& $scope.wgt && $scope.wgt != "" && $scope.pin && $scope.pin != "" && $scope.layout && $scope.layout != "" && $scope.surface && $scope.surface != "" 
+			&& $scope.bhposition && $scope.bhposition != "" && $scope.size && $scope.size != "" && $scope.depth && $scope.depth != "" && $scope.paph && $scope.paph != "" && $scope.papud && $scope.papud != "" 
+			&& $scope.papclt && $scope.papclt != "" && $scope.thumb && $scope.thumb != "" && $scope.fingers && $scope.fingers != "" && $scope.price && $scope.price != "" 
+			&& $scope.completed && $scope.completed != ""){
+				if($scope.a1 && $scope.a1 != "" && $scope.a2 && $scope.a2 != "" && $scope.a3 && $scope.a3 != "" && $scope.a4 && $scope.a4 != "" && $scope.a5 && $scope.a5 != "" && $scope.a6 && $scope.a6 != "" ){
+					if($scope.f1 && $scope.f1 != "" && $scope.f2 && $scope.f2 != "" && $scope.f3 && $scope.f3 != "" && $scope.f4 && $scope.f4 != "" && $scope.f5 && $scope.f5 != "" && $scope.f6 && $scope.f6 != "" && $scope.f7 && $scope.f7 != "" && $scope.f8 && $scope.f8 != "" && $scope.f9 && $scope.f9 != "" ){
+						if($scope.d1 && $scope.d1 != "" && $scope.d2 && $scope.d2 != "" && $scope.ft1 && $scope.ft1 != "" && $scope.ft2 && $scope.ft2 != "" && $scope.ft3 && $scope.ft3 != ""){
+							factory.createInfo({
+								Name: $scope.name,
+								DateCreated : $scope.date,
+								Address : $scope.address,
+								City : $scope.city,
+								State : $scope.state,
+								Zip : $scope.zip,
+								Phone : $scope.phone,
+								Cell : $scope.cell,
+								Email : $scope.email,
+								Hand : $scope.hand,
+								Conv : $scope.conv,
+								FT : $scope.ft,
+								Ball : $scope.ball,
+								Weight: $scope.wgt,
+								Pin : $scope.pin,
+								Layout : $scope.layout,
+								Surface : $scope.surface,
+								BHPosition : $scope.bhposition,
+								Size : $scope.size,
+								Depth : $scope.depth,
+								Paph : $scope.paph,
+								Papud : $scope.papud,
+								Papclt : $scope.papclt,
+								Thumb : $scope.thumb,
+								Fingers : $scope.fingers,
+								Price: $scope.price,
+								CompletedBy: $scope.completed,
+								Notes: $scope.notes,
+								A1: $scope.a1,
+								A2: $scope.a2,
+								A3: $scope.a3,
+								A4: $scope.a4,
+								A5: $scope.a5,
+								A6: $scope.a6,
+								CTC1: $scope.ctc1,
+								FS1: $scope.fs1,
+								CTC2: $scope.ctc2,
+								FS2: $scope.fs2,
+								F1: $scope.f1,
+								F2: $scope.f2,
+								F3: $scope.f3,
+								F4: $scope.f4,
+								F5: $scope.f5,
+								F6: $scope.f6,
+								F7: $scope.f7,
+								F8: $scope.f8,
+								F9: $scope.f9,
+								D1: $scope.d1,
+								D2: $scope.d2,
+								FT1: $scope.ft1,
+								FT2: $scope.ft2,
+								FT3: $scope.ft3
+							});
+							alert("Form successfully completed!");
+												
+						}
+						else{
+							alert("Please complete form");
+						}
+					}
+					else{
+						alert("Please complete form");
+					}
+				}
+				else{
+					alert("Please complete form");
+				}	
+			}
+			else{
+				alert("Please complete form");
+			}
+		}
+	}
+	]);
+	
+bapp.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('form');
+    });
+  };
+}])
+
+
+bapp.controller('searchCtrl', [
+'$scope',
+'factory',
+'auth',
+'searchService',
+'$state',
+function($scope, factory, auth, searchService, $state){
+	$scope.records = factory.factory;
+	if(searchService.getText() != '' && searchService.getText() != null){
+			$scope.name = searchService.getText().toLowerCase();
+
+	}
+	else
+		$scope.name = searchService.getText();
+
+	$scope.nameFilter = function(item) {
+		var regex = new RegExp($scope.name + ".*");
+		if($scope.name != '' && $scope.name) {
+			return regex.test(item.Name.toLowerCase());
+		} else {
+			return true;
+		}
+	}
+	$scope.search = function() {
+		searchService.setText($scope.searchtext);
+		$state.go($state.current, {}, {reload: true}); 
+	}
+}])
+
+bapp.controller('queryController', [
+'$scope',
+'factory',
+'searchService',
+'$state',
+function($scope, factory, searchService, $state){
+	$scope.search = function() {
+			searchService.setText($scope.searchtext);
+			$state.go('search');
+		}
+	$scope.name = factory.fetchHolder[0].Name;
+	$scope.date = factory.fetchHolder[0].DateCreated;
+	$scope.address = factory.fetchHolder[0].Address;
+	$scope.city = factory.fetchHolder[0].City;
+	$scope.state = factory.fetchHolder[0].State;
+	$scope.zip = factory.fetchHolder[0].Zip;
+	$scope.phone = factory.fetchHolder[0].Phone;
+	$scope.cell = factory.fetchHolder[0].Cell;
+	$scope.email = factory.fetchHolder[0].Email;
+	$scope.hand = factory.fetchHolder[0].Hand;
+	$scope.conv = factory.fetchHolder[0].Conv;
+	$scope.ft = factory.fetchHolder[0].FT;
+	$scope.ball = factory.fetchHolder[0].Ball;
+	$scope.wgt = factory.fetchHolder[0].Weight;
+	$scope.pin = factory.fetchHolder[0].Pin;
+	$scope.layout = factory.fetchHolder[0].Layout;
+	$scope.surface = factory.fetchHolder[0].Surface;
+	$scope.bhposition = factory.fetchHolder[0].BHPosition;
+	$scope.size = factory.fetchHolder[0].Size;
+	$scope.depth = factory.fetchHolder[0].Depth;
+	$scope.paph = factory.fetchHolder[0].Paph;						
+	$scope.papud = factory.fetchHolder[0].Papud;
+	$scope.papclt = factory.fetchHolder[0].Papclt;
+	$scope.thumb = factory.fetchHolder[0].Thumb;
+	$scope.fingers = factory.fetchHolder[0].Fingers;
+	$scope.price = factory.fetchHolder[0].Price;
+	$scope.completed = factory.fetchHolder[0].CompletedBy;
+	$scope.notes = factory.fetchHolder[0].Notes;
+	$scope.a1 = factory.fetchHolder[0].A1;
+	$scope.a2 = factory.fetchHolder[0].A2;
+	$scope.a3 = factory.fetchHolder[0].A3;
+	$scope.a4 = factory.fetchHolder[0].A4;
+	$scope.a5 = factory.fetchHolder[0].A5;
+	$scope.a6 = factory.fetchHolder[0].A6;
+	$scope.checkboxModel = {
+       value1 : false,
+       value2 : false,
+       value3 : false,
+       value4 : false
+     };
+	$scope.checkboxModel.value1 = factory.fetchHolder[0].CTC1;
+	$scope.checkboxModel.value2 = factory.fetchHolder[0].FS1;
+	$scope.checkboxModel.value3= factory.fetchHolder[0].CTC2;
+	$scope.checkboxModel.value4= factory.fetchHolder[0].FS2;
+	$scope.f1 = factory.fetchHolder[0].F1;
+	$scope.f2 = factory.fetchHolder[0].F2;
+	$scope.f3 = factory.fetchHolder[0].F3;
+	$scope.f4 = factory.fetchHolder[0].F4;
+	$scope.f5 = factory.fetchHolder[0].F5;
+	$scope.f6 = factory.fetchHolder[0].F6;
+	$scope.f7 = factory.fetchHolder[0].F7;
+	$scope.f8 = factory.fetchHolder[0].F8;
+	$scope.f9 = factory.fetchHolder[0].F9;
+
+	$scope.d1 = factory.fetchHolder[0].D1;
+	$scope.d2 = factory.fetchHolder[0].D2;
+	$scope.ft1 = factory.fetchHolder[0].FT1;
+	$scope.ft2 = factory.fetchHolder[0].FT2;
+	$scope.ft3 = factory.fetchHolder[0].FT3;
+}
+])
+
+    
+    
---
+++ server.js
@@ -0,0 +1,372 @@
+var url = require('url');
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var async = require('async');
+var bodyParser = require("body-parser");
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var ejwt = require('express-jwt');
+
+//create server
+var router = express();
+var server = http.createServer(router);
+var auth = ejwt({secret: 'SECRET', userProperty: 'payload'});
+
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
+router.use(bodyParser.json());
+router.use(passport.initialize());
+
+router.use(express.static(path.resolve(__dirname, 'public')));
+
+//this is where we actually turn to the outside world.  You'll need 
+//to adjust if you are on some other server
+server.listen(17049, "0.0.0.0", function() {
+    var addr = server.address();
+    console.log("listening to server");
+});
+
+//require mongoose
+var mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect("mongodb://localhost:29792/bowlingrecords", function(err, db) {
+    if (!err) {
+        console.log("We are connected to the database");
+    } else {
+        console.log("*** There was an error connecting to the database ***");
+    }
+});
+
+
+//set up table to store ball information
+var infoSchema = new mongoose.Schema({
+    Name: {
+        type: String,
+        required: true
+    },
+    DateCreated: {
+        type: String,
+        required: true
+    },
+    Address: {
+        type: String,
+        required: true
+    },
+    City: {
+        type: String,
+        required: true
+    },
+    State: {
+        type: String,
+        required: true
+    },
+    Zip: {
+        type: String,
+        required: true
+    },
+    Phone: {
+        type: String,
+        required: true
+    },
+    Cell: {
+        type: String
+    },
+    Email: {
+        type: String,
+        required: true
+    },
+    Hand: {
+        type: String,
+        required: true
+    },
+    Conv: {
+        type: String,
+        required: true
+    },
+    FT: {
+        type: String,
+        required: true
+    },
+    Ball: {
+        type: String,
+        required: true
+    },
+    Weight: {
+        type: String,
+        required: true
+    },
+    Pin: {
+        type: String,
+        required: true
+    },
+    Layout: {
+        type: String,
+        required: true
+    },
+    Surface: {
+        type: String,
+        required: true
+    },
+    BHPosition: {
+        type: String,
+        required: true
+    },
+    Size: {
+        type: String,
+        required: true
+    },
+    Depth: {
+        type: String,
+        required: true
+    },
+    Paph: {
+        type: String,
+        required: true
+    },
+    // Papv: {
+        // type: String,
+        // required: true
+    // },
+    Papud: {
+        type: String,
+        required: true
+    },
+    Papclt: {
+        type: String,
+        required: true
+    },
+    Thumb: {
+        type: String,
+        required: true
+    },
+    Fingers: {
+        type: String,
+        required: true
+    },
+    Price: {
+        type: String,
+        required: true
+    },
+    CompletedBy: {
+        type: String,
+        required: true
+    },
+    Notes: {
+        type: String,
+        required: true
+    },
+    A1: {
+        type: String,
+        required: true
+    },
+    A2: {
+        type: String,
+        required: true
+    },
+    A3: {
+        type: String,
+        required: true
+    },
+    A4: {
+        type: String,
+        required: true
+    },
+    A5: {
+        type: String,
+        required: true
+    },
+    A6: {
+        type: String,
+        required: true
+    },
+    CTC1: {
+        type: Boolean,
+    },
+    FS1: {
+        type: Boolean,
+    },
+    CTC2: {
+        type: Boolean,
+    },
+    FS2: {
+        type: Boolean,
+    },
+    F1: {
+        type: String,
+        required: true
+    },
+    F2: {
+        type: String,
+        required: true
+    },
+    F3: {
+        type: String,
+        required: true
+    },
+    F4: {
+        type: String,
+        required: true
+    },
+    F5: {
+        type: String,
+        required: true
+    },
+    F6: {
+        type: String,
+        required: true
+    },
+    F7: {
+        type: String,
+        required: true
+    },
+    F8: {
+        type: String,
+        required: true
+    },
+    F9: {
+        type: String,
+        required: true
+    },
+    D1: {
+        type: String,
+        required: true
+    },
+    D2: {
+        type: String,
+        required: true
+    },
+    FT1: {
+        type: String,
+        required: true
+    },
+    FT2: {
+        type: String,
+        required: true
+    },
+    FT3: {
+        type: String,
+        required: true
+    }
+
+})
+
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, unique: true},
+  hash: String,
+  salt: String
+});
+console.log("user schema created");
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+  return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+
+  // set expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, 'SECRET');
+};
+
+
+mongoose.model('User', UserSchema);
+
+mongoose.model('infoModel', infoSchema);
+
+var info = mongoose.model("infoModel");
+var User = mongoose.model('User');
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+function displayAllRecords(){
+info.find(function(err, records){
+  if(err){
+    console.log("*** error querying records! ***");
+  }
+})
+}
+
+//API
+
+router.post('/info', function(req, res, next) { 
+    var newInfo = new info(req.body);
+    newInfo.save(function(err, newInfo) {
+        if (err) {
+            console.log(err);
+            return next(err);
+        } else {
+            console.log("new personalInfo successfully saved!")
+             displayAllRecords();
+
+        }
+        res.json(newInfo);
+    });
+});
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
+
+router.get("/info", function(req, res, next){
+	info.find(function(err, records){
+		if (err) { return next(err); }
+		res.json(records);
+	});
+});
+
+router.get('/info/:ball', function(req, res, next) {
+	var url_parts = url.parse(req.url, true);
+	var str = url_parts.path;
+	var ObjectId = require('mongoose').Types.ObjectId; 
+	var id = str.substring(6);
+  info.find({"_id":id}, function(err, post) {
+    if (err) { return next(err); }
+    res.json(post);
+  });
+});
+
